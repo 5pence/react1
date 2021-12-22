82fe67328a7e2a26c7b3f8434282a3ce7f56d2f5
spencer barriball <sbarriball@me.com>
2021-12-22 03:49:25 +0000

    add forms for player names on Battle.js

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,21 +1,19 @@
 import * as React from 'react'
-import PropTypes from "prop-types";
-import { fetchPopularRepos } from "../utils/api";
-import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from "react-icons/fa";
+import PropTypes from 'prop-types'
+import { fetchPopularRepos } from '../utils/api'
+import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons/fa'
 
-function LanguagesNav ({selected, onUpdateLanguage}) {
-    /**
-     * Takes in what menu item was clicked and an updating function
-     */
-    const languages = ['All', 'JavaScript', 'Python', 'Ruby', 'Java', 'CSS', ]
+function LangaugesNav ({ selected, onUpdateLanguage }) {
+    const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
     return (
         <ul className='flex-center'>
             {languages.map((language) => (
                 <li key={language}>
-                    <button className='btn-clear nav-link'
-                            style={language === selected ? { color: 'red' } : null}
-                            onClick={() => onUpdateLanguage(language)}>
+                    <button
+                        className='btn-clear nav-link'
+                        style={language === selected ? { color: 'rgb(187, 46, 31)' } : null}
+                        onClick={() => onUpdateLanguage(language)}>
                         {language}
                     </button>
                 </li>
@@ -24,86 +22,88 @@ function LanguagesNav ({selected, onUpdateLanguage}) {
     )
 }
 
-// uses class PropTypes to declare types of attributes for function LanguagesNav, for safety, easier and fewer
-// overheads than using Typescript throughout
-LanguagesNav.propType = {
+LangaugesNav.propTypes = {
     selected: PropTypes.string.isRequired,
     onUpdateLanguage: PropTypes.func.isRequired
 }
 
-function ReposGrid({ repos }) {
+function ReposGrid ({ repos }) {
     return (
         <ul className='grid space-around'>
             {repos.map((repo, index) => {
-                const { name, owner, html_url, stargazers_count, forks, open_issues} = repo
+                const { name, owner, html_url, stargazers_count, forks, open_issues } = repo
                 const { login, avatar_url } = owner
 
                 return (
                     <li key={html_url} className='repo bg-light'>
-                        <h4 className='header-lg center-text'>#{index + 1}</h4>
-                        <img className='avatar' src={avatar_url} alt={`Avatar for ${login}`} />
+                        <h4 className='header-lg center-text'>
+                            #{index + 1}
+                        </h4>
+                        <img
+                            className='avatar'
+                            src={avatar_url}
+                            alt={`Avatar for ${login}`}
+                        />
                         <h2 className='center-text'>
                             <a className='link' href={html_url}>{login}</a>
                         </h2>
                         <ul className='card-list'>
                             <li>
                                 <FaUser color='rgb(255, 191, 116)' size={22} />
-                                <a href={`https://github.com/${login}`}> {login}</a>
+                                <a href={`https://github.com/${login}`}>
+                                    {login}
+                                </a>
                             </li>
                             <li>
-                                <FaStar color='rgb(255, 214, 0)' size={22} />
+                                <FaStar color='rgb(255, 215, 0)' size={22} />
                                 {stargazers_count.toLocaleString()} stars
                             </li>
                             <li>
-                                <FaCodeBranch color='rgb(129, 105, 245)' size={22} />
+                                <FaCodeBranch color='rgb(129, 195, 245)' size={22} />
                                 {forks.toLocaleString()} forks
                             </li>
                             <li>
                                 <FaExclamationTriangle color='rgb(241, 138, 147)' size={22} />
-                                {open_issues.toLocaleString()} open issues
+                                {open_issues.toLocaleString()} open
                             </li>
                         </ul>
                     </li>
                 )
-        })}
+            })}
         </ul>
     )
 }
 
-ReposGrid.proptype = {
+ReposGrid.propTypes = {
     repos: PropTypes.array.isRequired
 }
 
-
 export default class Popular extends React.Component {
     constructor(props) {
-        // as usual, call the parent constructor of extended class first
-        super(props);
+        super(props)
+
         this.state = {
             selectedLanguage: 'All',
             repos: {},
-            error: null
+            error: null,
         }
-        // now LanguagesNav is abstracted into a function we need this binding for state wiring
+
         this.updateLanguage = this.updateLanguage.bind(this)
         this.isLoading = this.isLoading.bind(this)
     }
-
-    componentDidMount() {
+    componentDidMount () {
         this.updateLanguage(this.state.selectedLanguage)
     }
-
-    updateLanguage(selectedLanguage) {
-        // using setState to update the value string of selectedLanguage
+    updateLanguage (selectedLanguage) {
         this.setState({
             selectedLanguage,
-            error: null
+            error: null,
         })
 
         if (!this.state.repos[selectedLanguage]) {
             fetchPopularRepos(selectedLanguage)
                 .then((data) => {
-                    this.setState(({repos}) => ({
+                    this.setState(({ repos }) => ({
                         repos: {
                             ...repos,
                             [selectedLanguage]: data
@@ -111,32 +111,33 @@ export default class Popular extends React.Component {
                     }))
                 })
                 .catch((error) => {
-                    console.warn('Error fetching them repos: ', error)
+                    console.warn('Error fetching repos: ', error)
+
                     this.setState({
-                        error: 'There was an error fetching the repositories from GitHub'
+                        error: `There was an error fetching the repositories.`
                     })
                 })
         }
     }
-
     isLoading() {
         const { selectedLanguage, repos, error } = this.state
+
         return !repos[selectedLanguage] && error === null
     }
-
     render() {
         const { selectedLanguage, repos, error } = this.state
 
         return (
-            /** remember React only like one top div wrapper - React.Fragment allows to stack many inside,
-             *  as would div, but React.Fragment is far more semantic and stops loads of extra divs on page
-             */
             <React.Fragment>
-                <LanguagesNav
+                <LangaugesNav
                     selected={selectedLanguage}
-                    onUpdateLanguage={this.updateLanguage}/>
+                    onUpdateLanguage={this.updateLanguage}
+                />
+
                 {this.isLoading() && <p>LOADING</p>}
-                {error && <p>{ error }</p>}
+
+                {error && <p>{error}</p>}
+
                 {repos[selectedLanguage] && <ReposGrid repos={repos[selectedLanguage]} />}
             </React.Fragment>
         )
