b81c60a2ac10ecdf649556ec0f40185215466715
spencer barriball <sbarriball@me.com>
2021-12-31 02:11:14 +0000

    add battle screen and resolve router

--- src/index.js
+++ src/index.js
@@ -2,21 +2,43 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular'
-import Battle from "./components/Battle";
-
+import Battle from './components/Battle'
+import { ThemeProvider } from './contexts/theme'
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className='container'>
-                {/*<Popular/>*/}
-                <Battle/>
-            </div>
-        )
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
     }
+  }
+  render() {
+    return (
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className='container'>
+              <Nav />
+
+              <Route exact path='/' component={Popular} />
+              <Route path='/battle' component={Battle} />
+            </div>
+          </div>
+        </ThemeProvider>
+      </Router>
+    )
+  }
 }
 
 ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
+  <App />,
+  document.getElementById('app')
 )
